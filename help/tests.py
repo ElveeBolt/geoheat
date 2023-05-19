4fd573f3fc49da8e51c6646da4e2dd4c68648e5c
Elvee Bolt <boltelvee@gmail.com>
2023-05-19 18:08:09 +0300

    Added TestCase for Help app

--- help/tests.py
+++ help/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+class TestCaseHelp(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_open_page(self):
+        response = self.client.get('/help/')
+        self.assertEqual(response.status_code, 200)
