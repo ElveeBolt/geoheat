dba4d0d7fb81051742bbdb5fe4163c1cc0f0f168
Elvee Bolt <boltelvee@gmail.com>
2023-05-19 20:37:40 +0300

    Fix url for location delete

--- location/urls.py
+++ location/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', LocationListView.as_view(), name='locations'),
     path('<int:pk>', LocationDetailView.as_view(), name='location'),
     path('<int:pk>/edit', LocationUpdateView.as_view(), name='location_edit'),
-    path('<int:pk>/detele', LocationDeleteView.as_view(), name='location_delete'),
+    path('<int:pk>/delete', LocationDeleteView.as_view(), name='location_delete'),
     path('<int:pk>/points', point_list_view, name='location_points'),
     path('create', LocationCreateView.as_view(), name='location_create'),
 
