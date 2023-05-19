05fc9400aa97c1dcb9bc660414d38d63857eb203
Elvee Bolt <boltelvee@gmail.com>
2023-05-19 18:10:09 +0300

    Added TestCase for Main app

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+class TestCaseMain(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_privacy(self):
+        response = self.client.get('/privacy/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_terms(self):
+        response = self.client.get('/terms/')
+        self.assertEqual(response.status_code, 200)
