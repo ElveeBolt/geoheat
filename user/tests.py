7b5934626430efbbe39211abfab9cc4e8fab623d
Elvee Bolt <boltelvee@gmail.com>
2023-05-20 08:26:59 +0300

    Added new TestCase for User app

--- user/tests.py
+++ user/tests.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
-from location.models import Location, Marker
 
 
 class UserTestCase(TestCase):
@@ -34,3 +33,18 @@ class UserTestCase(TestCase):
         response = self.client.get('/user/login/')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/')
+
+    def test_user_profile_not_auth(self):
+        response = self.client.get('/user/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/user/login?next=/user/')
+
+    def test_user_location_not_auth(self):
+        response = self.client.get('/locations/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/user/login?next=/locations/')
+
+    def test_user_change_password_not_auth(self):
+        response = self.client.get('/user/change_password/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/user/login?next=/user/change_password/')
